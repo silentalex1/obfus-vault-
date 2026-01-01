d94571536a8be24830156feaaf0edbc9cf3effed
Zal <101911964+silentalex1@users.noreply.github.com>
2025-12-31 22:21:14 -0500

    Update dashboard.js

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -10,7 +10,7 @@ const toggleText = document.getElementById('toggle-text');
 
 let isLoginMode = false;
 
-const CLIENT_ID = "1456100190300930154";
+const CLIENT_ID = "1456124894281597030";
 const REDIRECT_URI = encodeURIComponent("https://obfusvault.xyz/dashboard/main-page");
 const SCOPES = "identify";
 const DISCORD_AUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPES}`;
